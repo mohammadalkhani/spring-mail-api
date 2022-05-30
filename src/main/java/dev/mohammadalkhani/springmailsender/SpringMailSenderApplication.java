2649f798e33655f5b092bbedd78ccab79c894e6e
Mohammad al Khani <hello@mohammadalkhani.dev>
2022-05-30 14:01:48 -0700

    the applications hello world is completed

---
+++ src/main/java/dev/mohammadalkhani/springmailsender/SpringMailSenderApplication.java
@@ -0,0 +1,13 @@
+package dev.mohammadalkhani.springmailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMailSenderApplication.class, args);
+	}
+
+}
